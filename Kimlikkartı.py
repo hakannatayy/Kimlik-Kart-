455d31181397d3c6e8b26140d2f40b9fd7d61257
hakannatayy <48553376+hakannatayy@users.noreply.github.com>
2019-06-17 20:15:43 +0300

    Proje dosyası ekle.

---
+++ Kimlikkartı.py
@@ -0,0 +1,11 @@
+Adı="Hakan"
+Soyadi="ATAY"
+Dogumyeri="İzmir"
+Dogumtarih="1996"
+
+print("-------------------------------")
+print("Ad           =",Adı)
+print("Soyad        =",Soyadi)
+print("Doğum Yeri   =",Dogumyeri)
+print("Doğum Tarihi =",Dogumtarih)
+print("-------------------------------")
